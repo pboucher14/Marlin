80d668bc6e6b9da1032a17f2a7630584302f703c
Patrice Boucher <patrice_boucher@msn.com>
2021-10-27 16:28:01 -0400

    Merge bugfix-2.0.x into Ender3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(PBO, PBO_VER_4.2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(PBO, PBO_VER_4.3)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "PBO Ver 4.2"
+#define CUSTOM_MACHINE_NAME "PBO Ver 4.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -842,7 +842,7 @@
 #define I_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define K_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING true // PBO for bltouch // set to true to invert the logic of the probe.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING false // PBO for bltouch // set to true to invert the logic of the probe.
 
 
 /**
