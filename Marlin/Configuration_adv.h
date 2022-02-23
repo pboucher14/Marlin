5f957cd9e7e84d4553623209abd34fde6323764e
Patrice Boucher <patrice_boucher@msn.com>
2022-02-23 13:54:27 -0500

    Updated filament change load distance.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2496,7 +2496,7 @@
                                                   // 0 to disable start loading and skip to fast load only
   #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE  30  // PBO changed from 6 to 30. (mm/s) Load filament feedrate. This can be pretty fast.
   #define FILAMENT_CHANGE_FAST_LOAD_ACCEL     25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH   400  // PBO changed from 0 to 400
+  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH   350  // PBO changed from 0 to 350
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
   //#define ADVANCED_PAUSE_CONTINUOUS_PURGE       // Purge continuously up to the purge length until interrupted.
